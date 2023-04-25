f207edc2c3b494a70a1aa084505a05bc56ca51e9
stivennevits <stivennevits16@gmail.com>
2023-04-25 14:09:41 -0500

    se crean interfaces

---
+++ backend/bitech-challenge/src/main/java/com/challenge/ecommerce/bitechchallenge/controller/ProductoController.java
@@ -0,0 +1,15 @@
+package com.challenge.ecommerce.bitechchallenge.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/productos")
+public class ProductoController {
+	
+	@GetMapping("")
+	public String show() {
+		return "productos/show";
+	}
+}
---
+++ backend/bitech-challenge/src/main/java/com/challenge/ecommerce/bitechchallenge/service/ProductoService.java
@@ -0,0 +1,12 @@
+package com.challenge.ecommerce.bitechchallenge.service;
+
+import java.util.Optional;
+
+import com.challenge.ecommerce.bitechchallenge.model.Producto;
+
+public interface ProductoService {
+	public Producto save(Producto producto);
+	public Optional<Producto> get(Integer id);
+	public void update(Producto producto);
+	public void delete(Integer id);
+}
