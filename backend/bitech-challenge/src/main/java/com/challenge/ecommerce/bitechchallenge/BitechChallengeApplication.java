d9c72a7045f0e63401155d43270df292f1d8cc06
stivennevits <stivennevits16@gmail.com>
2023-04-25 11:34:49 -0500

    se crea estructura spring boot

---
+++ backend/bitech-challenge/src/main/java/com/challenge/ecommerce/bitechchallenge/BitechChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.ecommerce.bitechchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitechChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitechChallengeApplication.class, args);
+	}
+
+}
---
+++ backend/bitech-challenge/src/test/java/com/challenge/ecommerce/bitechchallenge/BitechChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.ecommerce.bitechchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitechChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
