fabc15fac86ec5e2ff101f28d80cbe37255cfca1
stivennevits <stivennevits16@gmail.com>
2023-04-25 19:22:10 -0500

    se implementa funcionalidad para guardar la imagen de producto

--- backend/bitech-challenge/src/main/java/com/challenge/ecommerce/bitechchallenge/controller/ProductoController.java
+++ backend/bitech-challenge/src/main/java/com/challenge/ecommerce/bitechchallenge/controller/ProductoController.java
@@ -1,5 +1,6 @@
 package com.challenge.ecommerce.bitechchallenge.controller;
 
+import java.io.IOException;
 import java.util.Optional;
 
 import org.slf4j.*;
@@ -10,10 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.challenge.ecommerce.bitechchallenge.model.Producto;
 import com.challenge.ecommerce.bitechchallenge.model.Usuario;
 import com.challenge.ecommerce.bitechchallenge.service.ProductoService;
+import com.challenge.ecommerce.bitechchallenge.service.UploadFileService;
 
 
 
@@ -25,6 +29,9 @@ public class ProductoController {
 	
 	@Autowired
 	private ProductoService productoService;
+	
+	@Autowired
+	private UploadFileService upload;
 	
 	@GetMapping("")
 	public String show(Model model) {
@@ -38,11 +45,26 @@ public class ProductoController {
 	}
 	
 	@PostMapping("/save")
-	public String save(Producto producto) {
+	public String save(Producto producto,@RequestParam("img") MultipartFile file) throws IOException {
 		LOGGER.info("Objeto producto {}", producto);
 		
 		Usuario u = new Usuario(1, "", "", "", "", "", "", "");
 		producto.setUsuario(u);
+		// logica para imagen
+		if(producto.getId() == null) { // cuando se crea el producto
+			String nombreImagen = upload.saveImage(file);
+			producto.setImagen(nombreImagen);
+		}else {
+			if (file.isEmpty()) { // cuando se edita p sin cambiar img
+				Producto p = new Producto();
+				p = productoService.get(producto.getId()).get();
+				producto.setImagen(p.getImagen());
+			}else {
+				String nombreImagen = upload.saveImage(file);
+				producto.setImagen(nombreImagen);
+			}
+		}
+		
 		productoService.save(producto);
 		return "redirect:/productos";
 	}
