71985156873c7e698d3b6b7a2a270db8966051cf
stivennevits <stivennevits16@gmail.com>
2023-04-25 22:03:22 -0500

    se crea ver producto

--- backend/bitech-challenge/src/main/java/com/challenge/ecommerce/bitechchallenge/controller/HomeController.java
+++ backend/bitech-challenge/src/main/java/com/challenge/ecommerce/bitechchallenge/controller/HomeController.java
@@ -1,17 +1,24 @@
 package com.challenge.ecommerce.bitechchallenge.controller;
 
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+import com.challenge.ecommerce.bitechchallenge.model.Producto;
 import com.challenge.ecommerce.bitechchallenge.service.ProductoService;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	private final Logger log=LoggerFactory.getLogger(HomeController.class);
 	
 	@Autowired
 	private ProductoService productoService;
@@ -22,4 +29,17 @@ public class HomeController {
 		model.addAttribute("productos", productoService.findAll());
 		return "usuario/home";
 	}
+	
+	@GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id, Model model) {
+		log.info("id producto enviado como parametro {}", id);
+		Producto producto = new Producto();
+		Optional<Producto> productoOptional = productoService.get(id);
+		producto = productoOptional.get();
+		
+		model.addAttribute("producto", producto);
+		
+		return "usuario/productoHome";
+	}
+	
 }
