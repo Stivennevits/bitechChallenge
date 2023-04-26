ce68bc3cd80ffffdd0c5579f52863f402d6a940f
stivennevits <stivennevits16@gmail.com>
2023-04-25 21:25:28 -0500

    se crea vista usuario y se corrigen containers de prod

--- backend/bitech-challenge/src/main/java/com/challenge/ecommerce/bitechchallenge/controller/HomeController.java
+++ backend/bitech-challenge/src/main/java/com/challenge/ecommerce/bitechchallenge/controller/HomeController.java
@@ -1,27 +1,25 @@
 package com.challenge.ecommerce.bitechchallenge.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.challenge.ecommerce.bitechchallenge.model.Producto;
+
 import com.challenge.ecommerce.bitechchallenge.service.ProductoService;
 
 @Controller
-@RequestMapping("/administrador")
-public class AdministradorController {
+@RequestMapping("/")
+public class HomeController {
 	
 	@Autowired
 	private ProductoService productoService;
 	
 	@GetMapping("")
 	public String home(Model model) {
-		List<Producto> productos = productoService.findAll();
-		model.addAttribute("productos", productos);
-		return "administrador/home";
+		
+		model.addAttribute("productos", productoService.findAll());
+		return "usuario/home";
 	}
 }
--- backend/bitech-challenge/src/main/java/com/challenge/ecommerce/bitechchallenge/controller/ProductoController.java
+++ backend/bitech-challenge/src/main/java/com/challenge/ecommerce/bitechchallenge/controller/ProductoController.java
@@ -55,14 +55,7 @@ public class ProductoController {
 			String nombreImagen = upload.saveImage(file);
 			producto.setImagen(nombreImagen);
 		}else {
-			if (file.isEmpty()) { // cuando se edita p sin cambiar img
-				Producto p = new Producto();
-				p = productoService.get(producto.getId()).get();
-				producto.setImagen(p.getImagen());
-			}else {
-				String nombreImagen = upload.saveImage(file);
-				producto.setImagen(nombreImagen);
-			}
+			
 		}
 		
 		productoService.save(producto);
@@ -80,13 +73,37 @@ public class ProductoController {
 	}
 	
 	@PostMapping("/update")
-	public String update(Producto producto) {
+	public String update(Producto producto,@RequestParam("img") MultipartFile file) throws IOException {
+		Producto p = new Producto();
+		p = productoService.get(producto.getId()).get();
+		
+		if (file.isEmpty()) { // cuando se edita p sin cambiar img
+			
+			producto.setImagen(p.getImagen());
+		}else {// cuando se edita la img
+			//eliminar cuando no sea img default
+			if (!p.getImagen().equals("default.jpg")) {
+				upload.deleteImage(p.getImagen());
+			}
+			String nombreImagen = upload.saveImage(file);
+			producto.setImagen(nombreImagen);
+		}
+		producto.setUsuario(p.getUsuario());
 		productoService.update(producto);
 		return "redirect:/productos";
 	}
 	
 	@GetMapping("/delete/{id}")
 	public String delete(@PathVariable Integer id) {
+		
+		Producto p = new Producto();
+		p = productoService.get(id).get();
+		
+		//eliminar cuando no sea img default
+		if (!p.getImagen().equals("default.jpg")) {
+			upload.deleteImage(p.getImagen());
+		}
+		
 		productoService.delete(id);
 		return "redirect:/productos";
 	}
